d854a8f6e5f0e9ce436c0b86f3f4cd1a07a39b55
XeraNight <XeraNight@users.noreply.github.com>
2026-02-25 17:45:30 +0000

    deploy: 74a278eab55470c7cf901dd5756aa895779aabf7

---
+++ _next/static/9n-ISQjhfgTwa8k50TaGQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fapp\u002Fleads\u002F[id]","\u002Fapp\u002Fstock\u002F[id]","\u002Fapp\u002Fstock\u002F[id]\u002Fedit"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
